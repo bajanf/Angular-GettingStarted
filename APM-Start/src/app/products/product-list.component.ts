30447a6331a41361f92dd07b92e228b82773caf0
Flavia Bajan <flavia.bajan@gmail.com>
2021-02-06 21:43:31 +0200

    work with dependency injection, retriving data using http, navigation and routing

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit} from '@angular/core';
 import { IProduct } from './product';
-
+import {ProductService } from './product.service'
 
 @Component({
-        selector: 'pm-products',
         templateUrl: './product-list.component.html',
         styleUrls:['./product-list.component.css',]
     })
@@ -24,33 +23,12 @@ export class ProductListComponent implements OnInit{
     }
 
     filteredProduct: IProduct[];
-    products: IProduct[]=[
-        {
-        "productId": 1,
-        "productName": "Leaf Rake",
-        "productCode": "GDN-0011",
-        "releaseDate": "March 19, 2019",
-        "description": "Leaf rake with 48-inch wooden handle.",
-        "price": 19.95,
-        "starRating": 3.2,
-        "imageUrl": "assets/images/leaf_rake.png"
-      },
-      {
-        "productId": 2,
-        "productName": "Garden Cart",
-        "productCode": "GDN-0023",
-        "releaseDate": "March 18, 2019",
-        "description": "15 gallon capacity rolling garden cart",
-        "price": 32.99,
-        "starRating": 4.2,
-        "imageUrl": "assets/images/garden_cart.png"
-      }
-    ];
+    products: IProduct[]=[];
+    errorMessage: string;
 
-    constructor()
+    constructor(private productService: ProductService )
     {
-        this.filteredProduct=this.products;
-        this.listFilter='cart';
+       // this.listFilter='cart';
     }
 
     performFilter(filterBy: string):IProduct[]
@@ -67,7 +45,14 @@ export class ProductListComponent implements OnInit{
     }
 
     ngOnInit():void{
-        console.log('in OnInit');
+        this.productService.getProducts().subscribe({
+            next: products => {
+                this.products=products,
+                this.filteredProduct=this.products;
+            },
+            error: err => this.errorMessage = err
+        });
+      
     }
 
     onRatingClicked(message: string):void
